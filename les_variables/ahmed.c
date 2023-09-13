7f2edb72cb063cfaf5f4fef884959214bde4c5d3
FOUDIAHMED <foudiahmed82@gmail.com>
2023-09-13 21:45:10 +0100

    ahmed

---
+++ les_variables/ahmed.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<stdbool.h>
+int main(){
+int age=22;
+float poid=77.5;
+char nom[]="ahmed";
+char prenom[]="foudi";
+bool est_humain=true;
+
+printf("\n votre nom est %s et votre prenom est %s\n",nom,prenom);
+printf("\n votre age est %d et votre poids est %.2f\n");
+}
