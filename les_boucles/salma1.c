74de44433d8e051665a4fb665703446d34c7ec15
salmaCoding <moussatef2001salma@gmail.com>
2023-09-14 12:47:18 +0200

    new file

---
+++ les_boucles/salma1.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+int main(){
+   char nom[10],prenom[10];
+   
+   printf("entrer votre nom et votre prenom\n");
+   scanf("%s,%s",&nom,&prenom);
+   
+   while(nom=="salma"&&prenom=="moussatef"){
+   	printf("bienvennue");
+   	
+   }
+	
+	printf("unkonwn");
+	
+}
