f702541ee3e032e9f419ccf28bf04530f4a89f40
FOUDIAHMED <foudiahmed82@gmail.com>
2023-09-14 11:53:24 +0100

    ahmed

---
+++ les_boucles/ahmed.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main(){
+int somme=0,i=1,n;
+ printf("entrer un nombre pour calculer la somme de 1 jusqua ce nombre \n");
+scanf("%d",&n);
+while(i++<=n)somme+=i;
+printf("la somme de 1 jusqu'a %d est %d",n,somme);
+}
+
