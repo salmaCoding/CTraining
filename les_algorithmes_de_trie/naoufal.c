bfcf6be96328d3796a5debecd0ffec1fb54e0515
NaoufalLabrihmi <naoufal.labrihmi@gmail.com>
2023-09-16 16:00:13 +0200

    naoufal

---
+++ les_algorithmes_de_trie/naoufal.c
@@ -0,0 +1,36 @@
+#include<stdio.h>
+int* tri(int T[],int N){
+	for(int i=0;i<N;i++)
+	{
+		printf("Donner moi le %d entier\n",i+1);
+		scanf("%d",&T[i]);
+	}
+		for(int i=0;i<N;i++)
+	{
+		for(int j=i+1;j<N;j++)
+		{
+			if(T[i]>T[j])
+			{
+				int temp;
+				temp=T[i];
+				T[i]=T[j];
+			 	T[j]=temp;	
+			}
+		}
+	}	
+	return T;
+}
+int main()
+{
+	int i,j,N,temp,T[N];
+	
+	printf("donner moi la taille du tableau :\n");
+	scanf("%d",&N);
+	tri(T,N);
+	printf("le tableau tri dans un orde croissant est :");
+	for(i=0;i<N;i++)
+	{
+		printf("%d\n",T[i]);
+	}
+	return 0;
+}
